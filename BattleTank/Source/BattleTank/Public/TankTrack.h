762599e24ad354539569ab1f3c720f5639912dbf
Juliano Carlos Mantovani Batista <julianomantovani1@hotmail.com>
2017-09-04 15:26:36 -0400

    154

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -18,6 +18,11 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	void SetThrottle(float Throttle);
+
+private:
 	
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float TrackMaxDrivingForce = 400000.0f;
 	
 };
